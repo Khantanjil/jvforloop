8262931167f00ce486a3d5e41db075e375124489
Tanjil <tanjilxd@gmail.com>
2020-07-18 09:43:17 +0100

    Add files via upload

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author tanjil
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x;
+        x = 20;
+        for(int i = 0; i<x; i++){
+            System.out.println(i);
+            if(i==x-1){
+                i++;
+                System.out.println(i);
+            }
+        }
+        
+        
+        
+    }
+    
+}
